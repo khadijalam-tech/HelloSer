a339ef9d78a143400573fa3e38ebeeac7ee4d60b
khadijalam <khadijalamhailik@gmail.com>
2025-10-04 19:04:52 +0100

    Initial commit

---
+++ src/main/java/com/example/helloservlet/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.example.helloservlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello from Servlet on WildFly!</h1>");
+    }
+}
